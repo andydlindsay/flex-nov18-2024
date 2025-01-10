e144a934c9d2fa334c91c9b25bb16d3627984f89
Andy Lindsay <andydlindsay@gmail.com>
2025-01-09 19:07:08 -0800

    add update functionality

--- m03w06-2/server.js
+++ m03w06-2/server.js
@@ -48,7 +48,7 @@ app.post('/pets', (req, res) => {
   // update the pets object with our new pet
   pets[id] = newPet;
 
-  console.log(pets);
+  // console.log(pets);
 
   // every successful POST request should end in a redirection
   // POST-REDIRECT-GET pattern
@@ -68,9 +68,39 @@ app.post('/pets/:petId', (req, res) => {
   res.redirect('/pets');
 });
 
+// Not covered during lecture
+// Update 
+// GET /pets/:petId/edit
+app.get('/pets/:petId/edit', (req, res) => {
+  // grab the petId from the url parameters
+  const petId = req.params.petId;
 
+  // create a templateVars object with the current pet information
+  const pet = pets[petId];
+  const templateVars = {
+    pet: pet
+  };
 
-// Update
+  // render the edit form
+  res.render('edit-form', templateVars);
+});
+
+// POST /pets/:petId/edit
+app.post('/pets/:petId/edit', (req, res) => {
+  // grab the pet id from the url parameters
+  const petId = req.params.petId;
+
+  // retrieve the pet from the pets object
+  const pet = pets[petId];
+
+  // update the object with the information from the body
+  pet.name = req.body.name;
+  pet.breed = req.body.breed;
+  pet.age = Number(req.body.age); // convert the age string to a number
+
+  // redirect the user back to /pets
+  res.redirect('/pets');
+});
 
 app.listen(port, () => {
   console.log(`app is listening on port ${port}`);
